818c2c74547a1730edef462298df9d05730a4761
Diego Bloise <diego-dev@outlook.com>
2026-05-13 11:36:05 -0300

    add: java jdbc - delete

--- javaJdbc/src/db/DB.java
+++ javaJdbc/src/db/DB.java
@@ -9,6 +9,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
+import db.exceptions.DbException;
+
 public class DB {
 
     private static Connection conn = null;
---
+++ javaJdbc/src/service/Delete.java
@@ -0,0 +1,35 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.exceptions.DbIntegrityException;
+
+public class Delete {
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        PreparedStatement statement = null;
+
+        try {
+            conn = DB.getConnection();
+            statement = conn.prepareStatement(
+                    "DELETE FROM department " +
+                            "WHERE id = ?");
+
+            statement.setInt(1, 5);
+
+            int rowAffected = statement.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowAffected);
+
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
+        } finally {
+            DB.closeStatement(statement);
+            DB.closeConnection();
+        }
+    }
+}
