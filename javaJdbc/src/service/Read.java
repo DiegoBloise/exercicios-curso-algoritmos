2175d4434380b33d6c140c6866a042319008d37e
Diego Bloise <diego-dev@outlook.com>
2026-05-13 11:24:19 -0300

    add: java jdbc - read

---
+++ javaJdbc/src/service/Read.java
@@ -0,0 +1,37 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+
+public class Read {
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            conn = DB.getConnection();
+
+            statement = conn.createStatement();
+
+            resultSet = statement.executeQuery("SELECT * FROM department");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("id") + ", " + resultSet.getString("Name"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            DB.closeResultSet(resultSet);
+            DB.closeStatement(statement);
+            DB.closeConnection();
+        }
+    }
+}
