33cf5c2d76fb8fea0c1272962f51d55333beaa76
Diego Bloise <diego-dev@outlook.com>
2026-05-19 11:02:23 -0300

    update

--- springboot/projeto1/src/main/java/com/curso/projeto1/model/Order.java
+++ springboot/projeto1/src/main/java/com/curso/projeto1/model/Order.java
@@ -17,6 +17,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,18 +26,22 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @EqualsAndHashCode.Include
     private Instant moment;
 
+    @EqualsAndHashCode.Include
     private Integer orderStatus;
 
     @ManyToOne
@@ -59,42 +64,5 @@ public class Order implements Serializable {
 
     public void setOrderStatus(OrderStatus orderStatus) {
         this.orderStatus = orderStatus.getCode();
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((moment == null) ? 0 : moment.hashCode());
-        result = prime * result + ((orderStatus == null) ? 0 : orderStatus.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Order other = (Order) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (moment == null) {
-            if (other.moment != null)
-                return false;
-        } else if (!moment.equals(other.moment))
-            return false;
-        if (orderStatus == null) {
-            if (other.orderStatus != null)
-                return false;
-        } else if (!orderStatus.equals(other.orderStatus))
-            return false;
-        return true;
     }
 }
