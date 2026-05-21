b2181da2b2739c7b7f2a57ceb60111bcb4341374
Diego Bloise <diego-dev@outlook.com>
2026-05-21 09:09:34 -0300

    update: database connection

--- springboot/projeto2/src/main/java/com/curso/projeto2/Projeto2Application.java
+++ springboot/projeto2/src/main/java/com/curso/projeto2/Projeto2Application.java
@@ -2,12 +2,13 @@ package com.curso.projeto2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class Projeto2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Projeto2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Projeto2Application.class, args);
+    }
 
 }
