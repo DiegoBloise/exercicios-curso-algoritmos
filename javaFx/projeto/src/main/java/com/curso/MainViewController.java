c421f3ad07c7cd7a053d6db3f4b7ab6cecb8e578
diegobloise-qwa <diego.bloise@qwasolucoes.com.br>
2026-05-25 13:13:38 -0300

    add: main view controller

--- javaFx/projeto/src/main/java/com/curso/MainViewController.java
+++ javaFx/projeto/src/main/java/com/curso/MainViewController.java
@@ -1,5 +1,40 @@
 package com.curso;
 
-public class MainViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction() {
+        System.out.println("Seller Action");
+    }
+
+    @FXML
+    public void onMenuItemDepartmentAction() {
+        System.out.println("Department Action");
+    }
+
+    @FXML
+    public void onMenuItemAboutAction() {
+        System.out.println("About Action");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
