2408c54cb8414ebf1796ad847e95415c09b690f0
Diego Bloise <diego-dev@outlook.com>
2026-05-26 12:38:36 -0300

    add: new department dialog

--- javaFx/projeto/src/main/java/com/curso/controller/DepartmentController.java
+++ javaFx/projeto/src/main/java/com/curso/controller/DepartmentController.java
@@ -1,22 +1,30 @@
 package com.curso.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import com.curso.App;
 import com.curso.model.Department;
 import com.curso.service.DepartmentService;
 import com.curso.util.Alerts;
+import com.curso.util.Utils;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class DepartmentController implements Initializable {
 
@@ -37,8 +45,8 @@ public class DepartmentController implements Initializable {
     private Button btnNew;
 
     @FXML
-    public void onBtnAction() {
-
+    public void onBtnAction(ActionEvent event) {
+        createDialogForm("department-form", Utils.currentStage(event));
     }
 
     @Override
@@ -61,6 +69,23 @@ public class DepartmentController implements Initializable {
         List<Department> departments = service.findAll();
         obsDepartments = FXCollections.observableArrayList(departments);
         tableViewDepartment.setItems(obsDepartments);
+    }
+
+    private void createDialogForm(String fxml, Stage parentStage) {
+        try {
+            Parent parent = App.loadFXML(fxml);
+
+            Stage dialoStage = new Stage();
+            dialoStage.setTitle("Enter Department data");
+            dialoStage.setScene(new Scene(parent));
+            dialoStage.setResizable(false);
+            dialoStage.initOwner(parentStage);
+            dialoStage.initModality(Modality.WINDOW_MODAL);
+            dialoStage.showAndWait();
+        } catch (IOException e) {
+            System.out.println("Erro ao criar dialog: " + e.getMessage());
+            Alerts.showAlert("Erro ao criar dialog", null, e.getMessage(), AlertType.ERROR);
+        }
     }
 
     public void setDepartmentService(DepartmentService service) {
